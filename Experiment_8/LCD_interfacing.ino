ed02efcd9e486055873514e16f0af5d766c51602
Bahubali P Fuladi <bpfuladi@gmail.com>
2021-05-20 15:02:16 +0530

    Add files via upload

---
+++ Experiment_8/LCD_interfacing.ino
@@ -0,0 +1,24 @@
+/****************************************************
+****** AIM : LCD interfacing with Atmega328 uC
+*****************************************************/
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 2, en = 3, d4 = 8, d5 = 9, d6 = 10, d7 = 11;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
